aa7b79227fac2b0865fa28859a6b0980e8444f7b
Uk-jake <mag0225@naver.com>
2023-05-18 14:30:19 -0400

    Register Page

--- app/src/main/java/com/dji/sdk/venture/MainActivity.java
+++ app/src/main/java/com/dji/sdk/venture/MainActivity.java
@@ -1,224 +1,202 @@
 package com.dji.sdk.venture;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import org.bouncycastle.jcajce.provider.symmetric.ARC4;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import dji.common.error.DJIError;
-import dji.common.error.DJISDKError;
-import dji.sdk.base.BaseComponent;
-import dji.sdk.base.BaseProduct;
-import dji.sdk.sdkmanager.DJISDKInitEvent;
-import dji.sdk.sdkmanager.DJISDKManager;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getName();
-    public static final String  FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
-    private static BaseProduct mProduct;
-    private Handler mHandler; // TODO instantiate (example code in sample code)
 
-    private Button mBtnOpen;
-    private static final String[] REQUIRED_PERMISSION_LIST = new String[] {
-            android.Manifest.permission.VIBRATE, // Gimbal rotation
-            android.Manifest.permission.INTERNET, // API requests
-            android.Manifest.permission.ACCESS_WIFI_STATE, // WIFI connected products
-            android.Manifest.permission.ACCESS_COARSE_LOCATION, // Maps
-            android.Manifest.permission.ACCESS_NETWORK_STATE, // WIFI connected products
-            android.Manifest.permission.ACCESS_FINE_LOCATION, // Maps
-            android.Manifest.permission.CHANGE_WIFI_STATE, // Changing between WIFI and USB connection
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE, // Log files
-            android.Manifest.permission.BLUETOOTH, // Bluetooth connected products
-            android.Manifest.permission.BLUETOOTH_ADMIN, // Bluetooth connected products
-            android.Manifest.permission.READ_EXTERNAL_STORAGE, // Log files
-            android.Manifest.permission.READ_PHONE_STATE, // Device UUID accessed upon registration
-            Manifest.permission.RECORD_AUDIO // Speaker accessory
-    };
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, AdapterView.OnItemSelectedListener{
+    Handler handler = new Handler();
+    Runnable runnable;
+    int loadIntervals = 100; //in ms. 1000ms = 1s
 
-    private List<String> missingPermission = new ArrayList<>();
-    private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
-    private static final int REQUEST_PERMISSION_CODE = 12345;
+    private GoogleMap mMap;
+    ArrayList<Drone> dronesToTrack = new ArrayList<>();
+    Drone selectedDrone;
+    Button trackDronebtn;
+    TextView tspidata;
+    TSPI mTSPI;
+    TSPIlogger mTSPIlogger;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        mBtnOpen = (Button) findViewById(R.id.buttonopen);
-        mBtnOpen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                MainActivity.this.startActivity(intent);
-            }
-        });
-        checkAndRequestPermissions();
-    }
-
-    private void checkAndRequestPermissions() {
-        // Check for permissions
-        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
-            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
-                missingPermission.add(eachPermission);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if
+            (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
             }
         }
-        // Request for missing permissions
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            showToast("Need to grant the permissions!");
-            ActivityCompat.requestPermissions(this,
-                    missingPermission.toArray(new String[missingPermission.size()]),
-                    REQUEST_PERMISSION_CODE);
+        Spinner spinner = findViewById(R.id.idSpinner);
+        spinner.setOnItemSelectedListener(this);
+        String[] droneIDs = new String[dronesToTrack.size()];
+        for (int i = 0; i < dronesToTrack.size(); i++) {
+            droneIDs[i] = dronesToTrack.get(i).getId();
         }
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+        ArrayAdapter ad
+                = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                droneIDs);
+
+        // set simple layout resource file
+        // for each item of spinner
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spinner.setAdapter(ad);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        tspidata = (TextView) findViewById(R.id.TSPIView);
+
+        mTSPI = new TSPI();
+        mTSPIlogger = new TSPIlogger(mTSPI);
+        mTSPIlogger.start();
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Check for granted permission and remove from missing list
-        if (requestCode == REQUEST_PERMISSION_CODE) {
-            for (int i = grantResults.length - 1; i >= 0; i--) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    missingPermission.remove(permissions[i]);
-                }
-            }
-        }
-        // If there is enough permission, we will start the registration
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else {
-            showToast("Missing permissions!!!");
-        }
-    }
-    private void startSDKRegistration() {
-        if (isRegistrationInProgress.compareAndSet(false, true)) {
-            AsyncTask.execute(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d("register","registering started");
-                    showToast("registering, pls wait...");
-                    DJISDKManager.getInstance().registerApp(MainActivity.this.getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
-
-                        @Override
-                        public void onRegister(DJIError djiError) {
-                            if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
-                                showToast("Register Success");
-
-                                // TODO commented for now so it doesn't download
-                                DJISDKManager.getInstance().startConnectionToProduct();
-
-                            } else {
-                                showToast("Register sdk fails, please check the bundle id and network connection!" + djiError.getDescription());
-                            }
-                            Log.v(TAG, djiError.getDescription());
-                        }
-
-                        @Override
-                        public void onProductDisconnect() {
-                            Log.d(TAG, "onProductDisconnect");
-                            showToast("Product Disconnected");
-                            notifyStatusChange();
-                        }
-                        @Override
-                        public void onProductConnect(BaseProduct baseProduct) {
-
-                            Log.d(TAG, String.format("onProductConnect newProduct:%s", baseProduct));
-                            showToast("Product Connected:"+baseProduct);
-                            notifyStatusChange();
-                            // start home activity when product is connected
-                            // UNCOMMENT IT ONLY IF THE DEVICE IS CONNECTED TO THE DRONE.
-
-//                                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-//                                MainActivity.this.startActivity(intent);
-
-                        }
-
-                        @Override
-                        public void onProductChanged(BaseProduct baseProduct) {
-                            //showToast("hello from onProductChanged");
-                            notifyStatusChange();
-                        }
-
-
-                        @Override
-                        public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
-                                                      BaseComponent newComponent) {
-
-                            if (newComponent != null) {
-                                newComponent.setComponentListener(new BaseComponent.ComponentListener() {
-
-                                    @Override
-                                    public void onConnectivityChange(boolean isConnected) {
-                                        Log.d(TAG, "onComponentConnectivityChanged: " + isConnected);
-                                        notifyStatusChange();
-                                    }
-                                });
-                            }
-                            Log.d(TAG,
-                                    String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s",
-                                            componentKey,
-                                            oldComponent,
-                                            newComponent));
-
-                        }
-                        @Override
-                        public void onInitProcess(DJISDKInitEvent djisdkInitEvent, int i) {
-                            //showToast("hello from init status");
-                        }
-
-                        @Override
-                        public void onDatabaseDownloadProgress(long l, long l1) {
-                            //showToast("hello from datadownload");
-                        }
-                    });
-                }
-            });
-        }
-    }
-    private void notifyStatusChange() {
-        //showToast("hello from notify status");
-        //mHandler.removeCallbacks(updateRunnable);
-        //mHandler.postDelayed(updateRunnable, 500);
-    }
-
-    private Runnable updateRunnable = new Runnable() {
-        @Override
-        public void run() {
-            //showToast("Hello");
-            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-            sendBroadcast(intent);
-        }
-    };
-
-    private void showToast(final String toastMsg) {
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
+    protected void onResume() {
+        handler.postDelayed(runnable = new Runnable() {
+            //update location of our drone every loadIntervals seconds.
             @Override
             public void run() {
-                Toast.makeText(getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show();
+
+                //batteryView.setText(batteryResidual);
+                handler.postDelayed(runnable,loadIntervals);
+                mMap.clear();
+                //tspidata.setText(mTSPI.logResults());
+                LatLng position = new LatLng(mTSPI.getCurrentLatitude(),mTSPI.getCurrentLongitude());//TODO calibrate coordinates here when you get the data from the drone.
+                //new LatLng(lat,lon);
+                Log.d("mapMarker","adding " + position);
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(position);
+                markerOptions.draggable(false);
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                //markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.defensivedrone)); // 1.2cm*1.2cm TODO must adjust to middle.
+
+                //below are rotations
+                //markerOptions.anchor(0.0f,0.0f);
+                //float angle = (float)mTSPI.getYaw();
+                //markerOptions.rotation(angle-20);
+                //rotation ends
+
+                mMap.addMarker(markerOptions);
+                //mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+                Log.d("mapMarker",position +" added");
             }
-        });
+        },loadIntervals);
+        super.onResume();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)  {
+        Log.d("Map", "Map ready.");
+        mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if
+            (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+        }
+        mMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        setDroneMarkers();
+
+        long minTime = 1000; //millisecond
+        float minDistance = 0;
+
+        // this location listener is to the device this application is running on only
+        // to follow a different device, we will need a check box or radio button of some sort to
+        // either pick to follow the good drone or enemy drone
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+                LatLng curPoint = new LatLng(latitude, longitude);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint,16));
+                locationManager.removeUpdates(this);
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance,  listener);
+    }
+
+    public void setDroneMarkers() {
+        for (int i = 0; i < dronesToTrack.size(); i++ ) {
+            Drone drone = dronesToTrack.get(i);
+            LatLng location = drone.getLocation();
+            String markerTitle = drone.getId();
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(location);
+            markerOptions.title(markerTitle);
+            markerOptions.draggable(false);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            mMap.addMarker(markerOptions);
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        selectedDrone = dronesToTrack.get(position);
+        trackDronebtn.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        trackDronebtn.setVisibility(View.INVISIBLE);
+    }
+
+    private void markCurrentLocation(LatLng point){
+
     }
 }
