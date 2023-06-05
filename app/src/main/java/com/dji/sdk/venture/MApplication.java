37d26b322b4786aa5157517a062ad507163a3a72
Uk-jake <mag0225@naver.com>
2023-06-05 15:34:14 -0400

    Update

--- app/src/main/java/com/dji/sdk/venture/MApplication.java
+++ app/src/main/java/com/dji/sdk/venture/MApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.secneo.sdk.Helper;
 
-import java.io.File;
-
 public class MApplication extends Application{
     @Override
     protected void attachBaseContext(Context paramContext){
